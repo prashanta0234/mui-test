d7754a333ffea29900d44deae0e7fcf159e61375
prashanta0234 <prashanta0234@gmail.com>
2022-09-25 02:37:58 +0600

    solve issue1

--- src/component/SideBar.js
+++ src/component/SideBar.js
@@ -1,96 +1,111 @@
 import React, { useState } from "react";
-import { Button, Typography } from "@mui/material";
+import {
+  Button,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import { Box } from "@mui/system";
 
-import deshbord from "../images/dashboard.svg";
+import dashbord from "../images/dashboard.svg";
 import aboutI from "../images/aboutI.svg";
 import "./style/Sidebar.css";
 
 import logout from "../images/logout.svg";
-import { BrowserRouter, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function SideBar() {
-  const [state, setState] = useState("deshbord");
+  const [state, setState] = useState("dashbord");
   const handleOnlick = (name) => {
     setState(name);
   };
 
   return (
     <>
-      <Box sx={{ mx: 1, position: "relative", minHeight: "85vh" }}>
+      <List sx={{ position: "relative", minHeight: "85vh" }}>
+        {/* reserve button */}
         <Link to="/reserve">
-          <Button
-            onClick={() => handleOnlick("deshbord")}
+          <ListItem
             sx={{
               color: "#7B61FF",
               width: "100%",
-              justifyContent: "flex-start",
+
               position: "relative",
               height: "44px",
-              my: "6px",
             }}
-            className={state === "deshbord" && "active"}
-            sentenceCase
           >
-            <img alt="" src={deshbord} height="20px" />
-            <Typography
-              sx={{ fontSize: "12px", textTransform: "none", ml: "8px" }}
+            <ListItemButton
+              onClick={() => handleOnlick("dashbord")}
+              className={state === "dashbord" && "active"}
+              sentenceCase
             >
-              Reserve
-            </Typography>
+              <ListItemIcon style={{ minWidth: "30px" }}>
+                <img alt="" src={dashbord} />
+              </ListItemIcon>
+              <ListItemText
+                primary=" Reserve"
+                primaryTypographyProps={{ fontSize: 12, fontWeight: "medium" }}
+              />
 
-            <Box
-              className={state !== "deshbord" && "box"}
-              sx={{
-                width: "2px",
-                height: "16px",
-                background: "#7B61FF",
-                borderRadius: "1000px",
-                position: "absolute",
-                right: "0px",
-              }}
-              placement="right"
-            />
-          </Button>
+              <Box
+                className={state !== "dashbord" && "box"}
+                sx={{
+                  width: "2px",
+                  height: "16px",
+                  background: "#7B61FF",
+                  borderRadius: "1000px",
+                  position: "absolute",
+                  right: "0px",
+                }}
+                placement="right"
+              />
+            </ListItemButton>
+          </ListItem>
         </Link>
         {/* About button */}
         <Link to="/about">
-          <Button
-            onClick={() => handleOnlick("about")}
-            className={state === "about" && "active"}
+          <ListItem
             sx={{
               color: "#7B61FF",
               width: "100%",
-              justifyContent: "flex-start",
+
               position: "relative",
               height: "44px",
-              my: "6px",
             }}
-            sentenceCase
           >
-            <img alt="" src={aboutI} height="20px" />
-            <Typography
-              sx={{ fontSize: "12px", textTransform: "none", ml: "8px" }}
+            <ListItemButton
+              onClick={() => handleOnlick("about")}
+              className={state === "about" && "active"}
+              sentenceCase
             >
-              About Us
-            </Typography>
+              <ListItemIcon style={{ minWidth: "30px" }}>
+                <img alt="" src={aboutI} />
+              </ListItemIcon>
+              <ListItemText
+                primary="About Us"
+                primaryTypographyProps={{ fontSize: 12, fontWeight: "medium" }}
+              />
 
-            <Box
-              className={state !== "about" && "box"}
-              sx={{
-                width: "2px",
-                height: "16px",
-                background: "#7B61FF",
-                borderRadius: "1000px",
-                position: "absolute",
-                right: "0px",
-              }}
-              placement="right"
-            />
-          </Button>
+              <Box
+                className={state !== "about" && "box"}
+                sx={{
+                  width: "2px",
+                  height: "16px",
+                  background: "#7B61FF",
+                  borderRadius: "1000px",
+                  position: "absolute",
+                  right: "0px",
+                }}
+              />
+            </ListItemButton>
+          </ListItem>
         </Link>
+
         {/* logout */}
-        <Button
+        {/* <Button
           sx={{
             color: "black",
             width: "100%",
@@ -110,8 +125,32 @@ export default function SideBar() {
           >
             Logout
           </Typography>
-        </Button>
-      </Box>
+        </Button> */}
+
+        <ListItem
+          sx={{
+            color: "black",
+            width: "100%",
+            justifyContent: "flex-start",
+            position: "absolute",
+
+            bottom: "16px",
+            left: "5px",
+            height: "44px",
+            my: "6px",
+          }}
+        >
+          <ListItemButton sentenceCase>
+            <ListItemIcon style={{ minWidth: "30px" }}>
+              <img alt="" src={logout} height="20px" />
+            </ListItemIcon>
+            <ListItemText
+              primary="Logout"
+              primaryTypographyProps={{ fontSize: 12, fontWeight: "medium" }}
+            />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </>
   );
 }
